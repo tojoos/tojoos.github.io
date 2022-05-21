62c07dcb139fcdc575e7314ea59251ae8594c184
ToJoos <joosx2000@gmail.com>
2022-05-21 10:48:59 +0200

    Finished project's layout. Added first project. Renamed classes in css/js. Removed unused stylings.

--- assets/js/main.js
+++ assets/js/main.js
@@ -171,13 +171,13 @@
   }
 
   /**
-   * Testimonials slider
+   * Projects slider
    */
-  new Swiper('.testimonials-slider', {
+  new Swiper('.projects-slider', {
     speed: 600,
     loop: true,
     autoplay: {
-      delay: 500000, // to change
+      delay: 10000,
       disableOnInteraction: false
     },
     slidesPerView: 'auto',
@@ -196,58 +196,6 @@
         slidesPerView: 1,
         spaceBetween: 20
       }
-    }
-  });
-
-  /**
-   * Porfolio isotope and filter
-   */
-  window.addEventListener('load', () => {
-    let portfolioContainer = select('.portfolio-container');
-    if (portfolioContainer) {
-      let portfolioIsotope = new Isotope(portfolioContainer, {
-        itemSelector: '.portfolio-item',
-        layoutMode: 'fitRows'
-      });
-
-      let portfolioFilters = select('#portfolio-flters li', true);
-
-      on('click', '#portfolio-flters li', function(e) {
-        e.preventDefault();
-        portfolioFilters.forEach(function(el) {
-          el.classList.remove('filter-active');
-        });
-        this.classList.add('filter-active');
-
-        portfolioIsotope.arrange({
-          filter: this.getAttribute('data-filter')
-        });
-      }, true);
-    }
-
-  });
-  GLightbox({
-    selector: '.portfolio-lightbox'
-  });
-  GLightbox({
-    selector: '.portfolio-details-lightbox',
-    width: '90%',
-    height: '90vh'
-  });
-  /**
-   * Portfolio details slider
-   */
-  new Swiper('.portfolio-details-slider', {
-    speed: 400,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
     }
   });
 
